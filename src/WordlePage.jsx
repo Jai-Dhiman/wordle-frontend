61d88fe9dbc7674f8a461068f1bb7c6ec952d7fb
JaiDhiman <jaidhiman2000@gmail.com>
2024-10-14 17:34:51 -0700

    gameboard

--- src/WordlePage.jsx
+++ src/WordlePage.jsx
@@ -1,9 +1,19 @@
-import { WordleIndex } from "./WordleIndex";
+// import { WordleIndex } from "./WordleIndex";
+import { useState } from "react";
+import GameBoard from "./GameBoard";
 
 export function WordlePage() {
+  const Game = () => {
+    const [guesses, setGuesses] = useState([]);
+    const [currentGuess, setCurrentGuess] = useState("");
+    const wordLength = 5;
+  };
+
   return (
     <main>
-      <WordleIndex />
+      <div className="game">
+        <GameBoard guesses={guesses} currentGuess={currentGuess} wordLength={wordLength} />
+      </div>
     </main>
   );
 }
